d194d960ae2532b846445cffff4a88954d0208c0
Mo-hwary <mohamedhwarycs94@gmail.com>
2025-06-13 03:37:34 +0300

    Simple projects

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -16,27 +16,56 @@ namespace Calculator
                 Console.WriteLine("Choose an operation: +, -, *, /");
                 string operation = Console.ReadLine();
                 double result = 0;
-                switch (operation)
+                /*  if(operation == "+")
                 {
-                    case "+":
-                        result = num1 + num2;
-                        break;
-                    case "-":
-                        result = num1 - num2;
-                        break;
-                    case "*":
-                        result = num1 * num2;
-                        break;
-                    case "/":
-                        if (num2 != 0)
-                            result = num1 / num2;
-                        else
-                            Console.WriteLine("Error: Division by zero is not allowed.");
-                        return; // Exit if division by zero
-                    default:
-                        Console.WriteLine("Invalid operation.");
-                        return; // Exit if invalid operation
+                    result = num1 + num2;
                 }
+                else if (operation == "-")
+                {
+                    result = num1 - num2;
+                }
+                else if (operation == "*")
+                {
+                    result = num1 * num2;
+                }
+                else if (operation == "/")
+                {
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                        continue; // Skip to the next iteration
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation.");
+                    continue; // Skip to the next iteration
+                }*/
+                /*  switch (operation)
+                  {
+                      case "+":
+                          result = num1 + num2;
+                          break;
+                      case "-":
+                          result = num1 - num2;
+                          break;
+                      case "*":
+                          result = num1 * num2;
+                          break;
+                      case "/":
+                          if (num2 != 0)
+                              result = num1 / num2;
+                          else
+                              Console.WriteLine("Error: Division by zero is not allowed.");
+                          return; // Exit if division by zero
+                      default:
+                          Console.WriteLine("Invalid operation.");
+                          return; // Exit if invalid operation
+                  }*/
                 Console.WriteLine($"The result of {num1} {operation} {num2} is: {result}");
                 Console.WriteLine("Do you want to perform another calculation? (y/n)");
                 string answer = Console.ReadLine();
---
+++ GetMonth/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+
+namespace GetMonth
+{
+    class Program
+    {
+        static string GetMonthName(int Month)
+        {
+            string monthName;
+            switch (Month)
+            {
+                case 1:
+                    monthName = "January";
+                    break;
+                case 2:
+                    monthName = "February";
+                    break;
+                case 3:
+                    monthName = "March";
+                    break;
+                case 4:
+                    monthName = "April";
+                    break;
+                case 5:
+                    monthName = "May";
+                    break;
+                case 6:
+                    monthName = "June";
+                    break;
+                case 7:
+                    monthName = "July";
+                    break;
+                case 8:
+                    monthName = "August";
+                    break;
+                case 9:
+                    monthName = "September";
+                    break;
+                case 10:
+                    monthName = "October";
+                    break;
+                case 11:
+                    monthName = "November";
+                    break;
+                case 12:
+                    monthName = "December";
+                    break;
+                default:
+                    monthName = "Invalid Month Number";
+                    return null;
+            }
+            return monthName;
+        }
+        static void SayPeriod(int startMonth, int endMonth)
+        {
+            double Period =  startMonth-endMonth;
+            if (Period < 0)
+            {
+                Period =Math.Abs(Period);
+            }
+            Console.WriteLine($"There is period from ({startMonth}){GetMonthName(startMonth)} to ({endMonth}){GetMonthName(endMonth)} :{Period} Months");
+        }
+
+        static void Main(string[] args)
+        { 
+            Console.Write("First month (1-12): ");
+            int firstMonth = int.Parse(Console.ReadLine());
+
+            Console.Write("Second month (1-12): ");
+            int secondMonth = int.Parse(Console.ReadLine());
+
+            SayPeriod(firstMonth, secondMonth);
+        }
+    }
+}
